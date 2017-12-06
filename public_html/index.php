a41131bf9df5e8b9baa5147c96bc67dee1c9aa0f
enyerber franco <enyerverfranco@outlook.com>
2017-12-06 13:39:28 -0400

    cambio de version

--- public_html/index.php
+++ public_html/index.php
@@ -1,20 +1,13 @@
 <?php
-
 $vendor = "../vendor/autoload.php";
 
 include ($vendor);
+
 if (!class_exists("\\CcMvc"))
 {
     trigger_error("Porfavor instala CcMvc via composer ejecutando 'composer install --prefer-dist'", E_USER_ERROR);
 }
-//include("vendor/autoload.php");
 
-$app = CcMvc::Start("../protected/", "App CcMvc");
-/**
- * indico que el controlador index/eliminar podra ser llamado mediante eliminar esto es solo para demostrar 
- * el enrutamiento manual CcMvc normalmente enruta automaticamente los controladores pero se puede 
- * se puede tomar el mando completamente si es nesesario similar a laravel 
- */
-$app->Router->Route('/{method}', 'index/{method}')->has('index');
-$app->Run(); // inica el funcionamiento del framework
+$app = CcMvc::Start("../protected/", "Mi Aplicacion Web");
 
+$app->Run();
